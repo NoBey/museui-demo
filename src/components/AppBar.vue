317fe7f6b8c007551e834c167b90fe105c528659
lxj <783384903@qq.com>
2016-11-22 16:36:05 +0800

    fix appbar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -26,20 +26,5 @@
 </template>
 
 <style lang="less">
-.appbar-search-field{
-  color: #FFF;
-  margin-bottom: 0;
-  &.focus-state {
-    color: #FFF;
-  }
-  .mu-text-field-hint {
-    color: fade(#FFF, 54%);
-  }
-  .mu-text-field-input {
-    color: #FFF;
-  }
-  .mu-text-field-focus-line {
-    background-color: #FFF;
-  }
-}
+
 </style>
